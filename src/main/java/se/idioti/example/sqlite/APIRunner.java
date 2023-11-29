28785524a171a80cee128717c59fe73ebd007689
Filip Nordqvist <114819894+Dautabossy@users.noreply.github.com>
2023-11-29 16:29:13 +0100

    Box box!

--- src/main/java/se/idioti/example/sqlite/APIRunner.java
+++ src/main/java/se/idioti/example/sqlite/APIRunner.java
@@ -33,15 +33,21 @@ public class APIRunner {
 	// Denna ska returera en lista av Unicorns.
 	}
 
+	public void getUnicorn(int id){
+		storage.fetchUnicorn(id);
+		
+	}
+
 	public static void main(String[] args) throws Exception {
 		APIRunner runner = new APIRunner();
 		Javalin app = Javalin.create(config -> {});
 		// A demonstration of how to use co	de within an endpoint
 
 		app.get("/", ctx -> {
+
 			runner.getMyUnicorns();});  // Alltså metoden ovan
 		app.get("/{id}", ctx -> {
-
+			ctx.html("Du gav mig ett id: ");
 		});
 		app.post("/", ctx -> {
 
